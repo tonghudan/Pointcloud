a4111de1d4120591a3360d16d6a6ffc00e4bc894
tonghudan <152654059@qq.com>
2019-08-07 10:34:23 +0800

    submit

---
+++ PointCloud/Exceptions/PCDReaderException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PointCloud.Exceptions
+{
+
+    class PCDReaderException : PointCloudException
+    {
+        public PCDReaderException() { }
+
+        public PCDReaderException(String message) : base(message)
+        {
+        }
+    }
+}
